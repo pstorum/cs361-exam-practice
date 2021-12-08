59c7fdf5e97eadda0be26bea49a2d98bb6465a4c
Peter Storum <p-storum@hotmail.com>
2021-12-07 16:16:51 -0800

    Update main.rb
    fixed #1 Main.rb smells have been fixed

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
 # CS 361 Exam Practice
 
-class Thang
+class Box
 
-  def initialize(x, y, b)
-    @width = x
-    @height = y
-    @depth = b
+  def initialize(width, height, depth)
+    @width = width
+    @height = height
+    @depth = depth
   end
 
-  # Calculate the volume (w x h x d)
   def volume
-        return @width * @height / @depth
-end
+        return @width * @height * @depth
+  end
+
 end
 
 
-cube = Thang.new(10, 20, 30)
-puts cube.volume
+box = Box.new(10, 20, 30)
+puts box.volume
 
 
 
